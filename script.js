6c25e42b051ad76d3a0ad790956f2d2d8d38fd8f
bielR2384 <113519393+bielR2384@users.noreply.github.com>
2024-08-29 19:31:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-reultado");
 
+const perguntas = [
+    {
+        enunciado:"Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado:"Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+            ]
+    }    
+];
+{
+    enunciado:"Pergunta 3",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado:"Pergunta 4",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+}     {
+    enunciado:"Pergunta 5",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado:"Pergunta 6",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+}    
+let atual = 0;
+let perguntaAtual;
+
+function mostrapergunta() {
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;   
+}
+
+mostrapergunta();
